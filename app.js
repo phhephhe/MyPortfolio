4cf44fe3846632abac7ea89c007506e03673069a
phhephhe <beridzedavit07@gmail.com>
2022-12-27 02:41:03 +0400

    added third project

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ let body = document.getElementsByTagName("BODY")[0];
 let btn = document.getElementById('light');
 
 btn.addEventListener('click', (el) =>{
+    el.preventDefault()
     if(lightMode.content == "light"){
         lightMode.content = "dark";
-        btn.innerHTML = "Night"
+        btn.innerHTML = "Day"
         body.style.color = 'white'
     }else{
         lightMode.content = "light"
-        btn.innerHTML = "Day"   
+        btn.innerHTML = "Night"   
         body.style.color = '#4F4F4F'
     }
 })
